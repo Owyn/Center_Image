7ae5afde236e02a11d7307b0167e5e215568ae4b
Owyn <Owyn@users.noreply.github.com>
2021-12-27 22:55:26 +0300

    Fill to window hotkey (Tab, Middle Mouse), image loading custom color (not just white now)

--- CenterImage.user.js
+++ CenterImage.user.js
@@ -2,8 +2,8 @@
 // @name          Center Image
 // @namespace     CenterImage
 // @author        Owyn
-// @version       1.94
-// @description   Centers images (directly opened with browser)(firefox-like) with hotkeys (to resize or scroll)
+// @version       1.95
+// @description   Improved controls for images directly opened with your browser - hotkeys & resizing & visuals
 // @updateURL     https://github.com/Owyn/Center_Image/raw/master/CenterImage.user.js
 // @downloadURL   https://github.com/Owyn/Center_Image/raw/master/CenterImage.user.js
 // @supportURL    https://github.com/Owyn/Center_Image/issues
@@ -31,10 +31,11 @@ if (typeof GM_registerMenuCommand !== "undefined")
 	GM_registerMenuCommand("Center Image Configuration", cfg, "n");
 }
 
-var rescaled = false;
+var rescaled = 0;
 var iot = 0, iol = 0;
 var i = images[0];
 
+var theStyle;
 function makeimage()
 {
 	if(typeof cfg_js !== "string") {setTimeout(function() { makeimage(); }, 11); return false;} // lets wait for async
@@ -43,15 +44,18 @@ function makeimage()
 		document.body.bgColor = cfg_bgclr;
 		if(document.head){document.head.innerHTML = "";} // remove FireFox background
 	}
-	document.body.innerHTML = "<style>img { position: absolute; top: 0; right: 0; bottom: 0; left: 0; }</style>"; // center image
+	document.body.innerHTML = "<style id=img_style>img { position: absolute; top: 0; right: 0; bottom: 0; left: 0; background-color: "+cfg_bgclr+"}</style>"; // center image & change loading image color from white in Chrome
+	theStyle = document.body.querySelector("#img_style");
 	i.style = ""; // chrome has lots of crap there
 	document.body.style = "";
-	i.style.margin = "auto"; // center image
+	theStyle.sheet.rules[0].style.margin = "auto"; // center image
 	document.body.style.margin = "0px";
 	document.body.appendChild(i);
 	i.addEventListener("mousedown", onmousedown, true);
 	i.addEventListener("click", rescale, true);
+	i.addEventListener("auxclick", rescale, true);
 	window.addEventListener("keydown", onkeydown, true);
+	window.addEventListener("scroll", onscroll, false);
 	window.addEventListener("resize", onresize, true);
 	onVisibilityChange();
 }
@@ -69,119 +73,209 @@ function onVisibilityChange()
 }
 document.addEventListener("visibilitychange", onVisibilityChange);
 
-function onresize()
+var scrolledY, scrolledX;
+function onscroll()
 {
-	if(rescaled)
+	scrolledY = window.pageYOffset;
+	scrolledX = window.pageXOffset;
+}
+
+var FireFox = ((navigator.userAgent.indexOf('Firefox') != -1) ? true : false);
+function onresize() // doesn't let image change back to "fit to window" in chrome & in both helps keep scroll progress
+{
+	if(rescaled != 1)
 	{
-		rescaled = false;
-		rescale();
+		let wasFilled = (rescaled === 2 ? true : false);
+		rescaled = 1;
+		let chromeplsstopX, chromeplsstopY;
+		if(!FireFox){ chromeplsstopX = window.pageXOffset; chromeplsstopY = window.pageYOffset;}
+		rescale(null, wasFilled);
+		if(!FireFox){ window.scrollTo(chromeplsstopX,chromeplsstopY);}
 	}
 }
 
-function changecursor()
+function changeCursor()
 {
-	i.style.margin = "auto";
-	if(!rescaled && (((i.naturalHeight / window.devicePixelRatio).toFixed() == window.innerHeight && (i.naturalWidth / window.devicePixelRatio).toFixed() <= window.innerWidth) || ((i.naturalHeight / window.devicePixelRatio).toFixed() <= window.innerHeight && (i.naturalWidth / window.devicePixelRatio).toFixed() == window.innerWidth))) // one img dimension is equal to screen and other is the same or less than the screen
+	//i.style.margin = "auto";
+	if(rescaled === 0) // original
 	{
-		i.style.cursor = "";
-	}
-	else if((i.naturalHeight / window.devicePixelRatio).toFixed() > window.innerHeight || (i.naturalWidth / window.devicePixelRatio).toFixed() > window.innerWidth) // at least one img dimenion is bigger than the screen
-	{
-		if(rescaled)
+		if(orgImgWidth == window.innerWidth || orgImgHeight == window.innerHeight) // perfect fit, can't resize
+		{
+			i.style.cursor = "";
+		}
+		else if (orgImgWidth > window.innerWidth || orgImgHeight > window.innerHeight)
+		{
+			i.style.cursor = "zoom-out";
+		}
+		else
 		{
 			i.style.cursor = "zoom-in";
-			i.style.cursor = "-webkit-zoom-in";
+		}
+	}
+	else if(rescaled === 2) // fill
+	{
+		if(orgImgWidth == window.innerWidth && orgImgHeight == window.innerHeight) // perfect fit, can't resize
+		{
+			i.style.cursor = "";
+		}
+		else if (orgImgWidth > i.width)
+		{
+			i.style.cursor = "zoom-in";
 		}
 		else
 		{
 			i.style.cursor = "zoom-out";
-			i.style.cursor = "-webkit-zoom-out";
-			if((i.naturalHeight / window.devicePixelRatio).toFixed() > window.innerHeight) // image pushing out-of-screen fix
-			{
-				i.style.margin = "0px auto";
-			}
 		}
+	}
+	else // if(rescaled === 1) // fit
+	{
+		if(orgImgWidth == window.innerWidth || orgImgHeight == window.innerHeight) // perfect fit, can't resize
+		{
+			i.style.cursor = "";
+		}
+		else if (orgImgWidth > i.width)
+		{
+			i.style.cursor = "zoom-in";
+		}
+		else
+		{
+			i.style.cursor = "zoom-out";
+		}
+	}
+
+	if(i.height > window.innerHeight) // image pushing out-of-screen browser bug fix
+	{
+		theStyle.sheet.rules[0].style.margin = "0px auto";
 	}
 	else
 	{
-		if(rescaled)
-		{
-			i.style.cursor = "zoom-out";
-			i.style.cursor = "-webkit-zoom-out";
-		}
-		else
-		{
-			i.style.cursor = "zoom-in";
-			i.style.cursor = "-webkit-zoom-in";
-		}
+		theStyle.sheet.rules[0].style.margin = "auto";
 	}
+	i.style.margin = ""; // chrome pls stop
 }
 
 function onmousedown()
 {
 	if(i.offsetLeft > 0){iol = i.offsetLeft;}
 	if(i.offsetTop > 0){iot = i.offsetTop;}
+
+	if(event.which === 2) // middle mouse Chrome fix
+	{
+		event.preventDefault();
+		event.stopPropagation();
+		return;
+	}
 }
 
-function rescale(event)
+function rescale(event, fill)
 {
-	if(rescaled)
+	let ex,ey;
+	if(event) // mouse click
 	{
-		rescaled = false;
-		var scale, ex, ey;
-		if(event)
+		if (typeof event.y === "undefined") // Firefox
 		{
-			if (typeof event.y === "undefined") // Firefox
-			{
-				ex = event.clientX;
-				ey = event.clientY;
-			}
-			else
-			{
-				ex = event.x;
-				ey = event.y;
-			}
-			ex -= iol;
-			ey -= iot;
-			scale = Math.min((window.innerWidth / (i.naturalWidth / window.devicePixelRatio).toFixed()), (window.innerHeight / (i.naturalHeight / window.devicePixelRatio).toFixed()));
+			ex = event.clientX;
+			ey = event.clientY;
 		}
-		i.removeAttribute("width");
-		i.removeAttribute("height");
-		i.style.width = (i.naturalWidth / window.devicePixelRatio).toFixed() + "px";
-		i.style.height = (i.naturalHeight / window.devicePixelRatio).toFixed() + "px";
-		changecursor();
-		if(event)
+		else
 		{
-			window.scrollTo(ex / scale - window.innerWidth / 2, ey / scale - window.innerHeight / 2);
+			ex = event.x;
+			ey = event.y;
+		}
+		ex -= iol;
+		ey -= iot;
+		if(event.which === 2) // middle mouse
+		{
+			fill = true;
+		}
+		else if(event.which === 3) // right mouse
+		{
+			return;
+		}
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	document.body.style.overflowX = '';
+	document.body.style.overflowY = '';
+
+	let scrollMax_Y = window.scrollMaxY || ((document.body.scrollHeight || document.documentElement.scrollHeight)- document.documentElement.clientHeight);
+	let scrollMax_X = window.scrollMaxX || ((document.body.scrollWidth || document.documentElement.scrollWidth)- document.documentElement.clientWidth);
+
+	let scrollProgressY = scrolledY / scrollMax_Y;
+	let scrollProgressX = scrolledX / scrollMax_X;
+
+	let unFilling = false;
+
+	let sidesCMP;
+	if(fill)
+	{
+		if(rescaled === 2) // to original
+		{
+			rescaled = 0;
+			theStyle.sheet.rules[0].style.width = orgImgWidth + "px";
+			theStyle.sheet.rules[0].style.height = orgImgHeight + "px";
+		}
+		else // fill
+		{
+			sidesCMP = (orgImgWidth / orgImgHeight) < (window.innerWidth / window.innerHeight);
+			rescaled = 2;
 		}
 	}
 	else
 	{
-		i.removeAttribute("width");
-		i.removeAttribute("height");
-		i.style.width = (i.naturalWidth / window.devicePixelRatio).toFixed() + "px";
-		i.style.height = (i.naturalHeight / window.devicePixelRatio).toFixed() + "px";
-		if((i.naturalWidth / window.devicePixelRatio).toFixed() != window.innerWidth)
+		if(rescaled != 0) // to original
 		{
-			i.style.width = window.innerWidth + "px";
-			i.style.height = "";
-			rescaled = true;
+			if(rescaled === 2) {unFilling = true;}
+			rescaled = 0;
+			theStyle.sheet.rules[0].style.width = orgImgWidth + "px";
+			theStyle.sheet.rules[0].style.height = orgImgHeight + "px";
 		}
+		else // fit
+		{
+			sidesCMP = (orgImgWidth / orgImgHeight) > (window.innerWidth / window.innerHeight);
+			rescaled = 1;
+		}
+	}
+
+	if(rescaled != 0)
+	{
+		if(sidesCMP)
+		{
+			theStyle.sheet.rules[0].style.width = "100%";
+			theStyle.sheet.rules[0].style.height = "auto";
+			document.body.style.overflowX = 'hidden'; // we don't need unscrollable scrollbars if they appear
+		}
+		else
+		{
+			theStyle.sheet.rules[0].style.height = "100%";
+			theStyle.sheet.rules[0].style.width = "auto";
+			document.body.style.overflowY = 'hidden'; // we don't need unscrollable scrollbars if they appear
+		}
+	}
+
+	changeCursor();
+
+	if(event && (!unFilling && (!fill || (fill && (scrollMax_Y <= 0 && scrollMax_X <= 0))))) // left mouse click (fill-click with no scrollbars and not left click after middle click - else preserve scroll percentage)
+	{
+		let scale = Math.min((window.innerWidth / i.width), (window.innerHeight / i.height));
+		window.scrollTo((ex / scale) - (window.innerWidth / 2), (ey / scale) - (window.innerHeight / 2));
+	}
+	else // keep percentage scroll progress for KB hotkeys
+	{
 		
-		if((i.height > window.innerHeight) || (i.width > window.innerWidth))
-		{
-			i.style.width = (i.naturalWidth / window.devicePixelRatio).toFixed() + "px";
-			i.style.height = (i.naturalHeight / window.devicePixelRatio).toFixed() + "px";
-			if((i.naturalHeight / window.devicePixelRatio).toFixed() != window.innerHeight)
-			{
-				i.style.height = window.innerHeight + "px";
-				i.style.width = "";
-				rescaled = true;
-			}
-		}
-		changecursor();
+		scrollMax_Y = window.scrollMaxY || ((document.body.scrollHeight || document.documentElement.scrollHeight)- document.documentElement.clientHeight);
+		scrollMax_X = window.scrollMaxX || ((document.body.scrollWidth || document.documentElement.scrollWidth)- document.documentElement.clientWidth);
+
+		window.scrollTo(Math.round(scrollProgressX * scrollMax_X), Math.round(scrollProgressY * scrollMax_Y));
+
+		scrolledY = window.pageYOffset;
+		scrolledX = window.pageXOffset;
 	}
 }
+
+var orgImgWidth;
+var orgImgHeight;
 
 function autoresize()
 {
@@ -189,30 +283,46 @@ function autoresize()
 	{
 		document.lastChild.insertBefore(document.createElement("head"), document.body);
 	}
-	var link = document.createElement('link');
+	/*var link = document.createElement('link');
 	link.type = 'image/x-icon';
 	link.rel = 'shortcut icon';
 	link.href = i.src;
-	document.head.appendChild(link); // favicon
-	var title = i.src.substr(i.src.lastIndexOf("/")+1);
+	document.head.appendChild(link);*/ // favicon
+	/*var title = i.src.substr(i.src.lastIndexOf("/")+1);
 	if(title.indexOf("?") != -1)
 	{
 		title = title.substr(0, title.indexOf("?"));
 	}
-	document.title = title + " (" + i.naturalWidth + "x" + i.naturalHeight + ")"; // title
+	document.title = title + " (" + i.naturalWidth + "x" + i.naturalHeight + ")";*/ // title
 	
-	rescaled = true;rescale(0); // to original size in pixels
-	if(cfg_fitWH && i.height > window.innerHeight && i.width > window.innerWidth) // both scrollbars
+	orgImgWidth = Math.round(i.naturalWidth / window.devicePixelRatio);
+	orgImgHeight = Math.round(i.naturalHeight / window.devicePixelRatio);
+
+	let InitRescale = false; // directly opened image is already fit to window if it is bigger by the browser
+	if(cfg_fitWH && orgImgHeight > window.innerHeight && orgImgWidth > window.innerWidth) // both scrollbars
 	{
-		rescale(0);
+		InitRescale = false;
 	}
-	else if(cfg_fitB && (i.height > window.innerHeight || i.width > window.innerWidth)) // one scrollbar
+	else if(cfg_fitB && (orgImgHeight > window.innerHeight || orgImgWidth > window.innerWidth)) // one scrollbar
 	{
-		rescale(0);
+		InitRescale = false;
 	}
-	else if(cfg_fitS && i.height <= window.innerHeight && i.width <= window.innerWidth) // no scrollbars
+	else if(cfg_fitS && orgImgHeight <= window.innerHeight && orgImgWidth <= window.innerWidth) // no scrollbars
 	{
-		rescale(0);
+		InitRescale = true;
+	}
+	else
+	{
+		theStyle.sheet.rules[0].style.width = orgImgWidth + "px";
+		theStyle.sheet.rules[0].style.height = orgImgHeight + "px";
+	}
+	if(InitRescale)
+	{
+		rescale(null, cfg_fill ? true : false);
+	}
+	else
+	{
+		changeCursor();
 	}
 	if(cfg_js){eval(cfg_js);}
 }
@@ -236,7 +346,10 @@ if (typeof KeyEvent === "undefined")
 		DOM_VK_NUMPAD4: 100,
 		DOM_VK_NUMPAD5: 101,
 		DOM_VK_NUMPAD6: 102,
-		DOM_VK_NUMPAD8: 104
+		DOM_VK_NUMPAD8: 104,
+		DOM_VK_F5: 116,
+		DOM_VK_TAB: 9,
+		DOM_VK_ENTER: 13
 	};
 }
 
@@ -309,9 +422,14 @@ function onkeydown (b)
 		scroll_space(b.shiftKey, b.ctrlKey);
 		cancelEvent(b);
 		break;
+	case KeyEvent.DOM_VK_TAB:
+	case KeyEvent.DOM_VK_ENTER:
+		rescale(null, true);
+		cancelEvent(b);
+		break;
 	case KeyEvent.DOM_VK_Q:
 	case KeyEvent.DOM_VK_NUMPAD5:
-		rescale(0);
+		rescale(null, false);
 		cancelEvent(b);
 		break;
 	case KeyEvent.DOM_VK_P:
@@ -324,6 +442,7 @@ var cfg_bgclr = "grey";
 var cfg_fitWH = true;
 var cfg_fitB = false;
 var cfg_fitS = true;
+var cfg_fill = false;
 var cfg_js;
 
 function cfg(){}
@@ -336,6 +455,7 @@ if (typeof GM !== "undefined" && typeof GM.getValue !== "undefined")
 			cfg_fitWH = await GM.getValue("fitWH", true);
 			cfg_fitB = await GM.getValue("fitB", false);
 			cfg_fitS = await GM.getValue("fitS", true);
+			cfg_fill = await GM.getValue("fill", false);
 			cfg_js = await GM.getValue("js", "");
 		makeimage();
 	}
@@ -351,11 +471,16 @@ if (typeof GM !== "undefined" && typeof GM.getValue !== "undefined")
 			GM.setValue("fitWH", $("ci_cfg_3_fitWH").checked);
 			GM.setValue("fitB", $("ci_cfg_4_fitB").checked);
 			GM.setValue("fitS", $("ci_cfg_5_fitS").checked);
+			GM.setValue("fill", $("ci_cfg_7_fill").checked);
 			GM.setValue("js", $("ci_cfg_6_js").value);
 			alert("Configuration Saved");
 			if($("ci_cfg_2_bgclr").value){document.body.bgColor = $("ci_cfg_2_bgclr").value;}else{document.body.removeAttribute("bgColor");}
 		}
-		if(i){i.removeEventListener("click", rescale, true);}
+		if(i){
+			i.removeEventListener("click", rescale, true);
+			i.removeEventListener("auxclick", rescale, true);
+			i.removeEventListener("mousedown", onmousedown, true);
+		}
 		window.removeEventListener("keydown", onkeydown, true);
 		if(document.head){document.head.innerHTML = "";}
 		document.body.innerHTML = "";
@@ -363,10 +488,11 @@ if (typeof GM !== "undefined" && typeof GM.getValue !== "undefined")
 		div.style.margin = "11% auto";
 		div.style.width = "444px";
 		div.style.border = "solid 1px black";
+		div.style.color = "black";
 		div.style.background = "silver";
 		div.innerHTML = "<b><center>Configuration</center></b>"
 		+ "<br><input id='ci_cfg_2_bgclr' type='text' size='6'> Background color (empty = default)"
-		+ "<br><br>Fit to window images:"
+		+ "<br><br>Fit to window images:" + " ( Fill to window instead <input id='ci_cfg_7_fill' type='checkbox'> )"
 		+ "<br><br><input id='ci_cfg_3_fitWH' type='checkbox'> Larger than window both vertically and horizontally"
 		+ "<br><br><input id='ci_cfg_4_fitB' type='checkbox'> Larger than window either vertically or horizontally"
 		+ "<br><br><input id='ci_cfg_5_fitS' type='checkbox'> Smaller than window"
@@ -377,6 +503,7 @@ if (typeof GM !== "undefined" && typeof GM.getValue !== "undefined")
 		$("ci_cfg_3_fitWH").checked = cfg_fitWH;
 		$("ci_cfg_4_fitB").checked = cfg_fitB;
 		$("ci_cfg_5_fitS").checked = cfg_fitS;
+		$("ci_cfg_7_fill").checked = cfg_fill;
 		$("ci_cfg_6_js").value = cfg_js;
 		$("ci_cfg_save").addEventListener("click", saveCfg, true);
 	}
